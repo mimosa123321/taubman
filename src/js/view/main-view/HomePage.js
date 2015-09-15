4594e17e0db85a158c651eb0349b333cafe341e6
Mimosa Poon <mimosapoon0703@gmail.com>
2015-09-16 02:15:59 +0800

    update title

--- src/js/view/main-view/HomePage.js
+++ src/js/view/main-view/HomePage.js
@@ -50,7 +50,7 @@ var HomePage = module.exports = React.createClass({
     },
 
     submitForm:function() {
-        $.post("http://www.taubmanasia.com/china-mall-2020/download_whitepaper.aspx", function(result) {
+        $.post("/china-mall-2020/download_whitepaper_zh.aspx", function(result) {
 
         }.bind(this));
     },
@@ -126,7 +126,7 @@ var HomePage = module.exports = React.createClass({
 
 
 
-                <form className="emailForm" id="myEmailForm" method="POST" action="http://www.taubmanasia.com/china-mall-2020/download_whitepaper.aspx">
+                <form className="emailForm" id="myEmailForm" method="POST" action="/china-mall-2020/download_whitepaper_zh.aspx">
                     <div className = "emailFormBg"></div>
                     <div className = "closeButton" onClick={this.closeForm}><img src="build/images/closebtn.png" /></div>
                     <div className = "contents">
