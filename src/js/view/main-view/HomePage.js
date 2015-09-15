16b1d304e2ade47962dd17ef055c9e89b86f4f05
Mimosa Poon <mimosapoon0703@gmail.com>
2015-09-15 23:37:49 +0800

    update urls

--- src/js/view/main-view/HomePage.js
+++ src/js/view/main-view/HomePage.js
@@ -50,7 +50,7 @@ var HomePage = module.exports = React.createClass({
     },
 
     submitForm:function() {
-        $.post("http://chinamall.xenyo.net:8080/download_whitepaper.aspx", function(result) {
+        $.post("http://www.taubmanasia.com/china-mall-2020/download_whitepaper.aspx", function(result) {
 
         }.bind(this));
     },
@@ -124,7 +124,7 @@ var HomePage = module.exports = React.createClass({
                     </a>
                 </div>
 
-                <form className="emailForm" id="myEmailForm" method="POST" action="http://chinamall.xenyo.net:8080/download_whitepaper.aspx">
+                <form className="emailForm" id="myEmailForm" method="POST" action="http://www.taubmanasia.com/china-mall-2020/download_whitepaper.aspx">
                     <div className = "emailFormBg"></div>
                     <div className = "closeButton" onClick={this.closeForm}><img src="build/images/closebtn.png" /></div>
                     <div className = "contents">
